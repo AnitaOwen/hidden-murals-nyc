08bcd37e7d4a2bc370f94069ead66bb9fb476b65
AnitaOwen <anitaowen@pursuit.org>
2024-01-29 15:49:16 -0500

    added fetch for all murals

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,3 +1,7 @@
 // Murals
 const URL = import.meta.env.VITE_BASE_API_URL;
 
+// Index/Get all
+export function getAllMurals() {
+    return fetch(`${URL}/murals`).then((response) => response.json());
+  }
