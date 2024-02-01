616f2d7444ea81712e17470f998cd42b678fe5d4
AnitaOwen <anitaowen@pursuit.org>
2024-02-01 12:38:43 -0500

    added search bar form and state

--- src/components/MuralList.jsx
+++ src/components/MuralList.jsx
@@ -4,9 +4,12 @@ import Mural from './Mural'
 import Aside from "./Aside";
 
 const MuralList = ({allMurals}) => {
+  const [searchInput, setSearchInput] = useState("")
+
   const { borough } = useParams();
   const [selectedBorough, setSelectedBorough] = useState(borough || "")
-
+  
+  // Aside component boroughs onClick
   function handleBoroughClick(boro){
     setSelectedBorough(boro)
   }
@@ -14,14 +17,49 @@ const MuralList = ({allMurals}) => {
     // array of all mural objects that match the selectedBorough state or all murals.
     const filteredBorough = selectedBorough ? allMurals.filter((mural) => mural.location.borough === selectedBorough) : allMurals
     // console.log(filteredBorough)
+
+    // Search bar text input
+    function handleTextChange(event){
+      const input = event.target.value
+      setSearchInput(input)
+    }
+
+    // Filter murals based on search input
+    function filteredSearchMurals(){
+      return allMurals.filter((mural)=> {
+        const {artist, location, title, year} = mural
+        const artistMatch = artist.toLowerCase().match(searchInput.toLowerCase())
+        const neighborhoodMatch = location.neighborhood.toLowerCase().match(searchInput.toLowerCase())
+        const streetMatch = location.intersection.toLowerCase().match(searchInput.toLowerCase())
+        const titleMatch = title.toLowerCase().match(searchInput.toLowerCase())
+        const yearMatch = year.toString().includes(searchInput)
+        return artistMatch || neighborhoodMatch || streetMatch || titleMatch || yearMatch
+    })
+  }
+  const searchResults = filteredSearchMurals()
     
   return (
     <>
       <Aside handleBoroughClick={handleBoroughClick} />
+      <div>
+        <form>
+            <label htmlFor="searchInput">Search</label>
+            <div>
+                <input 
+                placeholder="Enter artist, title, location, or year"
+                type="search"
+                id="searchInput"
+                onChange={handleTextChange}
+                value={searchInput} 
+                />
+          </div>
+        </form>
+    </div>
       <main>
         {filteredBorough.map((mural) => (
-          <Mural key={mural.id} mural={mural}/>
+        <Mural key={mural.id} mural={mural}/>
         ))}
+
       </main>
     </>
   )
