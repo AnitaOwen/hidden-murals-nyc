15f9761402dfd3985142b7f8a241df01070aa505
Jazon Younge <jay@Jazons-MacBook-Air.local>
2024-02-02 16:37:38 -0400

    added MuralForm file

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import { useEffect, useState } from "react";
 import Home from "./components/Home";
 import MuralList from "./components/MuralList";
 import MuralInfo from "./components/MuralInfo";
-
+import MuralForm from "./components/MuralForm";
 import About from "./components/About";
-import { getAllMurals} from "./api/fetch"
+import { getAllMurals } from "./api/fetch";
 
-import "./App.css"
+import "./App.css";
 
-
-function App () {
-  const [allMurals, setAllMurals] = useState ([])
-  
+function App() {
+  const [allMurals, setAllMurals] = useState([]);
 
   // const [isDarkMode, setIsDarkMode] = useState(false)
 
@@ -30,33 +28,31 @@ function App () {
         console.error(error);
       });
   }, []);
-  
 
   return (
-
     // <div className={isDarkMode ? "dark-mode" : "light-mode"}>
     <>
       <Routes>
-    
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-    
+
         {/* JAZON: this is your route for the form view. Please fill in name of the component */}
-        {/* <Route path="/new" element={<name-of-your-component />} /> */}
-    
+        <Route path="mural/new" element={<MuralForm />} />
         <Route path="/murals">
-            <Route index element={<MuralList allMurals={allMurals} />} />
-            <Route path=":borough" element={<MuralList allMurals={allMurals} />} />
-          </Route>
-    
-        <Route path="/mural/:id" element={<MuralInfo allMurals={allMurals} />} />
-    
+          <Route index element={<MuralList allMurals={allMurals} />} />
+          <Route
+            path=":borough"
+            element={<MuralList allMurals={allMurals} />}
+          />
+        </Route>
+
+        <Route
+          path="/mural/:id"
+          element={<MuralInfo allMurals={allMurals} />}
+        />
       </Routes>
-
     </>
-
-
-  )
-};
+  );
+}
 
 export default App;
--- src/components/MuralList.jsx
+++ src/components/MuralList.jsx
@@ -1,73 +1,82 @@
 // import { useParams } from "react-router-dom";
 import { useState } from "react";
-import Mural from './Mural'
+import Mural from "./Mural";
 import Aside from "./Aside";
 
-const MuralList = ({allMurals}) => {
-  const [searchInput, setSearchInput] = useState("")
+const MuralList = ({ allMurals }) => {
+  const [searchInput, setSearchInput] = useState("");
 
   // const { borough } = useParams();
-  const [selectedBorough, setSelectedBorough] = useState("")
-  
+  const [selectedBorough, setSelectedBorough] = useState("");
+
   // Aside component boroughs onClick
-  function handleBoroughClick(boro){
-    setSelectedBorough(boro)
-    setSearchInput('')
+  function handleBoroughClick(boro) {
+    setSelectedBorough(boro);
+    setSearchInput("");
   }
 
-    // array of all mural objects that match the selectedBorough state or all murals.
-    const filteredBorough = selectedBorough ? allMurals.filter((mural) => mural.location.borough === selectedBorough) 
-    : allMurals
-    console.log(filteredBorough)
+  // array of all mural objects that match the selectedBorough state or all murals.
+  const filteredBorough = selectedBorough
+    ? allMurals.filter((mural) => mural.location.borough === selectedBorough)
+    : allMurals;
+  console.log(filteredBorough);
 
-    // Search bar text input
-    function handleTextChange(event){
-      const input = event.target.value
-      setSearchInput(input)
-    }
-
-    // Filter murals based on search input
-    function filteredSearchMurals(){
-      return allMurals.filter((mural)=> {
-        const {artist, location, title, year} = mural
-        const artistMatch = artist.toLowerCase().match(searchInput.toLowerCase())
-        const neighborhoodMatch = location.neighborhood.toLowerCase().match(searchInput.toLowerCase())
-        const streetMatch = location.intersection.toLowerCase().match(searchInput.toLowerCase())
-        const titleMatch = title.toLowerCase().match(searchInput.toLowerCase())
-        const yearMatch = year.toString().match(searchInput)
-        return artistMatch || neighborhoodMatch || streetMatch || titleMatch || yearMatch
-    })
+  // Search bar text input
+  function handleTextChange(event) {
+    const input = event.target.value;
+    setSearchInput(input);
   }
-  const searchResults = filteredSearchMurals()
-    
+
+  // Filter murals based on search input
+  function filteredSearchMurals() {
+    return allMurals.filter((mural) => {
+      const { artist, location, title, year } = mural;
+      const artistMatch = artist.toLowerCase().match(searchInput.toLowerCase());
+      const neighborhoodMatch = location.neighborhood
+        .toLowerCase()
+        .match(searchInput.toLowerCase());
+      const streetMatch = location.intersection
+        .toLowerCase()
+        .match(searchInput.toLowerCase());
+      const titleMatch = title.toLowerCase().match(searchInput.toLowerCase());
+      const yearMatch = year.toString().match(searchInput);
+      return (
+        artistMatch ||
+        neighborhoodMatch ||
+        streetMatch ||
+        titleMatch ||
+        yearMatch
+      );
+    });
+  }
+  const searchResults = filteredSearchMurals();
+
   return (
     <>
       <Aside handleBoroughClick={handleBoroughClick} />
       <div>
         <form>
-            <label htmlFor="searchInput">Search all murals: </label>
-            {/* <div> */}
-                <input 
-                placeholder="enter search term"
-                type="search"
-                id="searchInput"
-                onChange={handleTextChange}
-                value={searchInput} 
-                />
+          <label htmlFor="searchInput">Search all murals: </label>
+          {/* <div> */}
+          <input
+            placeholder="enter search term"
+            type="search"
+            id="searchInput"
+            onChange={handleTextChange}
+            value={searchInput}
+          />
           {/* </div> */}
         </form>
       </div>
       <main>
         {searchInput.length > 0
-          ? searchResults.map((mural) => (
-          <Mural key={mural.id} mural={mural}/>
-          ))
+          ? searchResults.map((mural) => <Mural key={mural.id} mural={mural} />)
           : filteredBorough.map((mural) => (
-          <Mural key={mural.id} mural={mural}/>
-        ))}
+              <Mural key={mural.id} mural={mural} />
+            ))}
       </main>
     </>
-  )
-}
+  );
+};
 
-export default MuralList
+export default MuralList;
