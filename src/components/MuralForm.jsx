2c28845d49ee3d2d353d137da6a1cb36e76a8211
Jazon Younge <jay@Jazons-MacBook-Air.local>
2024-02-05 13:02:18 -0400

    added forms, but those regarding location are malfunctioning

--- src/components/MuralForm.jsx
+++ src/components/MuralForm.jsx
@@ -39,14 +39,97 @@ function MuralForm() {
   //rendering
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Mural Name:</label>
-      <input
-        id="title"
-        type="text"
-        name="title"
-        value={newMural.title}
-        onChange={handleTextChange}
-      />
+      <div>
+        <div>
+          <label htmlFor="artist">Artist:</label>
+        </div>
+
+        <input
+          type="text"
+          id="artist"
+          value={newMural.artist}
+          onChange={handleTextChange}
+        />
+        <div></div>
+        <div>
+          <label htmlFor="title">Title:</label>
+        </div>
+
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={newMural.title}
+          onChange={handleTextChange}
+        />
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="neighborhood">Neighborhood:</label>
+        </div>
+        <input
+          type="text"
+          id="neighborhood"
+          value={newMural.location.neighborhood}
+          onChange={handleTextChange}
+        />
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="borough">Borough:</label>
+        </div>
+        <select
+          id="borough"
+          name="borough"
+          value={newMural.location.borough}
+          onChange={handleTextChange}
+        >
+          <option value="Manhattan">Manhattan</option>
+          <option value="Brooklyn">Brooklyn</option>
+          <option value="Queens">Queens</option>
+          <option value="Bronx">Bronx</option>
+          <option value="Staten Island">Staten Island</option>
+        </select>
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="intersection">Intersection:</label>
+        </div>
+        <input
+          type="text"
+          id="intersection"
+          value={newMural.location.intersection}
+          onChange={handleTextChange}
+        />
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="year">Year:</label>
+        </div>
+        <input
+          type="text"
+          id="year"
+          value={newMural.year}
+          onChange={handleTextChange}
+        />
+      </div>
+
+      <div>
+        <div>
+          <label htmlFor="description">Description:</label>
+        </div>
+        <input
+          type="text"
+          id="description"
+          value={newMural.description}
+          onChange={handleTextChange}
+        />
+      </div>
+
       <button type="submit">Submit</button>
     </form>
   );
