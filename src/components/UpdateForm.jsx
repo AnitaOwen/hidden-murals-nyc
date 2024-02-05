cf96521d89c1ba2ae6c574c0311627abc4d0a48f
AnitaOwen <anitaowen@pursuit.org>
2024-02-05 12:04:04 -0500

    added back button to edit mural form

--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { editMural, getOneMural } from "../api/fetch";
+import { Link } from "react-router-dom";
 import "./UpdateForm.css"
 const UpdateForm = () => {
 
@@ -49,100 +50,108 @@ const UpdateForm = () => {
   }, [id]);
 
   return (
-    <form onSubmit={handleUpdateSubmit}>
-      <img src={updateMural.image} alt="Hidden Mural" />
-      <div className="label-input">
-        <div>
-          <label htmlFor="artist">Artist:</label>
+    <>
+      {/* <Link to={`/mural/${id}`}>
+            <p>Go Back</p>
+          </Link> */}
+      <form onSubmit={handleUpdateSubmit}>
+      <Link to={`/mural/${id}`}>
+            <p>Go Back</p>
+          </Link>
+        <img src={updateMural.image} alt="Hidden Mural" />
+        <div className="label-input">
+          <div>
+            <label htmlFor="artist">Artist:</label>
+          </div>
+          <input
+            type="text"
+            id="artist"
+            value={updateMural.artist}
+            onChange={handleTextChange}
+          />
         </div>
-        <input
-          type="text"
-          id="artist"
-          value={updateMural.artist}
-          onChange={handleTextChange}
-        />
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="title">Title:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="title">Title:</label>
+          </div>
+          <input
+            type="text"
+            id="title"
+            value={updateMural.title}
+            onChange={handleTextChange}
+          />
         </div>
-        <input
-          type="text"
-          id="title"
-          value={updateMural.title}
-          onChange={handleTextChange}
-        />
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="neighborhood">Neighborhood:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="neighborhood">Neighborhood:</label>
+          </div>
+          <input
+            type="text"
+            id="neighborhood"
+            value={updateMural.location.neighborhood}
+            onChange={handleTextChange}
+          />
         </div>
-        <input
-          type="text"
-          id="neighborhood"
-          value={updateMural.location.neighborhood}
-          onChange={handleTextChange}
-        />
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="borough">Borough:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="borough">Borough:</label>
+          </div>
+          <select
+            id="borough"
+            name="borough"
+            value={updateMural.borough}
+            onChange={handleTextChange}
+          >
+            <option value="Manhattan">Manhattan</option>
+            <option value="Brooklyn">Brooklyn</option>
+            <option value="Queens">Queens</option>
+            <option value="Bronx">Bronx</option>
+            <option value="Staten Island">Staten Island</option>
+          </select>
         </div>
-        <select
-          id="borough"
-          name="borough"
-          value={updateMural.borough}
-          onChange={handleTextChange}
-        >
-          <option value="Manhattan">Manhattan</option>
-          <option value="Brooklyn">Brooklyn</option>
-          <option value="Queens">Queens</option>
-          <option value="Bronx">Bronx</option>
-          <option value="Staten Island">Staten Island</option>
-        </select>
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="intersection">Intersection:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="intersection">Intersection:</label>
+          </div>
+          <input
+            type="text"
+            id="intersection"
+            value={updateMural.location.intersection}
+            onChange={handleTextChange}
+          />
         </div>
-        <input
-          type="text"
-          id="intersection"
-          value={updateMural.location.intersection}
-          onChange={handleTextChange}
-        />
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="year">Year:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="year">Year:</label>
+          </div>
+          <input
+            type="text"
+            id="year"
+            value={updateMural.year}
+            onChange={handleTextChange}
+          />
         </div>
-        <input
-          type="text"
-          id="year"
-          value={updateMural.year}
-          onChange={handleTextChange}
-        />
-      </div>
 
-      <div className="label-input">
-        <div>
-          <label htmlFor="description">Description:</label>
+        <div className="label-input">
+          <div>
+            <label htmlFor="description">Description:</label>
+          </div>
+          <input
+            type="text"
+            id="description"
+            value={updateMural.description}
+            onChange={handleTextChange}
+            className="description-input" />
         </div>
-        <input
-          type="text"
-          id="description"
-          value={updateMural.description}
-          onChange={handleTextChange}
-          className="description-input" />
-      </div>
 
-      <button>Submit Changes</button>
-    </form>
+        <button>Submit Changes</button>
+      </form>
+    </>
   )
 }
 
